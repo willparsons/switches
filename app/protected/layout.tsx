3dd320252a0439b93ad85ac0146fbd14e27ef7b1
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-16 22:18:21 +0100

    remove dead code

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/components/deploy-button";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import Link from "next/link";
@@ -10,10 +9,7 @@ export default function ProtectedLayout({ children }: { children: React.ReactNod
                 <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                     <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                         <div className="flex gap-5 items-center font-semibold">
-                            <Link href={"/"}>Next.js Supabase Starter</Link>
-                            <div className="flex items-center gap-2">
-                                <DeployButton />
-                            </div>
+                            <Link href={"/"}>Switches</Link>
                         </div>
                         <AuthButton />
                     </div>
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 
 export default async function ProtectedPage() {
     const supabase = await createClient();
@@ -28,7 +27,6 @@ export default async function ProtectedPage() {
             </div>
             <div>
                 <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-                <FetchDataSteps />
             </div>
         </div>
     );
