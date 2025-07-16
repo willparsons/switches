7e7cdf0858b68fb7da707cbfd9d51d3abdb4c68d
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-16 18:37:30 +0100

    remove tutorial stuff

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
@@ -27,7 +25,6 @@ export default function Home() {
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
         </div>
 
