15efe0863e8cbddbab4526ec30307c6fdf1fd256
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-16 18:41:21 +0100

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    printWidth: 120,
+}
+
+export default config
