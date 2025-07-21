8462bde79927d7ad9399cb5ef0d2e082160322b8
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-21 22:47:15 +0100

    add more useful types to types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,6 @@
-import type { Database } from "@/database.types";
+import { type Database, Constants } from "@/database.types";
 
 export type Switch = Database["public"]["Tables"]["switches"]["Row"];
+
+export const Types = Constants.public.Enums.Type;
+export const TypesNoUnknown = Types.filter((t) => t !== "Unknown");
