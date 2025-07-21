e2ea829a12adfca8232b1db5d24a0512b090e8cf
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-21 22:46:15 +0100

    add hall effect styling to the switch card

--- components/SwitchCard.tsx
+++ components/SwitchCard.tsx
@@ -17,6 +17,8 @@ function getTypeColor(type: Switch["Type"]) {
             return "bg-blue-100 text-blue-800";
         case "Clicky":
             return "bg-green-100 text-green-800";
+        case "Hall Effect":
+            return "bg-purple-100 text-purple-800";
         default:
             return "bg-gray-100 text-gray-800";
     }
