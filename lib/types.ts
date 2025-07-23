822b7e8fd159a1198709dab4bd5dc680db80a70a
willparsons <williamparsons62@gmail.com>
2025-07-23 19:55:52 +0100

    Fix filter bar lag (#13)

--- lib/types.ts
+++ lib/types.ts
@@ -2,5 +2,5 @@ import { type Database, Constants } from "@/database.types";
 
 export type Switch = Database["public"]["Tables"]["switches"]["Row"];
 
-export const Types = Constants.public.Enums.Type;
-export const TypesNoUnknown = Types.filter((t) => t !== "Unknown");
+export const SwitchTypes = Constants.public.Enums.Type;
+export const SwitchTypesNoUnknown = SwitchTypes.filter((t) => t !== "Unknown");
